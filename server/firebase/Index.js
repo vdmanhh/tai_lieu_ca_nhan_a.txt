a208f9fdfc8f2d6e8a035317b7045ffb598e2586
= <=>
2021-08-30 19:02:04 +0700

    faile

--- server/firebase/Index.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../config/fbAccountService.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-module.exports=admin
--- server/router/cloudinary.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const {uploadd,removed} = require('../controller/cloudinary')
-const {adminCheck,authCheck} = require('../mildware/auth')
-router.post("/uploadimages",authCheck ,adminCheck,uploadd)
-router.post("/removeimages",authCheck ,adminCheck,removed)
-
-module.exports = router;
